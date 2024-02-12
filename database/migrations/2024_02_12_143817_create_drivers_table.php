b39d43c59e89fa823fd77bd6bef7c053e6bb6344
Cawe541 <cawe.wewe04@gmail.com>
2024-02-12 11:44:39 -0300

    update(driver): converting cpf column in unique

--- database/migrations/2024_02_12_143817_create_drivers_table.php
+++ database/migrations/2024_02_12_143817_create_drivers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('drivers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('cpf', 11);
+            $table->string('cpf', 11)->unique();
             $table->timestamps();
         });
     }
