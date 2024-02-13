41571f2cce139b4ccb2c9adcce44245e85f22560
Cawe541 <cawe.wewe04@gmail.com>
2024-02-13 13:17:33 -0300

    update(pallets): changing the name to singular

--- app/Models/Pallet.php
+++ app/Models/Pallet.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Pallets extends Model
+class Pallet extends Model
 {
     use HasFactory;
 }
